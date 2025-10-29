9eaad4271e189276bba3d6ecbef8badc4e72b4e5
Karin Knutsson <karinmiriam@gmail.com>
2025-10-29 15:49:02 +0100

    added glsl plugin

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import glsl from "vite-plugin-glsl";
 // import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    glsl(),
     // vueDevTools(),
   ],
   resolve: {
